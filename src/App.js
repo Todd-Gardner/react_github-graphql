9a4ff6d448c77e3e12537a803f62dac681e2f018
Todd-Gardner <Todd-Gardner@users.noreply.github.com>
2022-01-14 13:45:15 -0500

    ADDED: useEffect fetch to get github user info

--- src/App.js
+++ src/App.js
@@ -1,4 +1,28 @@
+import { useEffect } from "react";
+import github from "./db";
+
 function App() {
+  useEffect(() => {
+    const githubQuery = {
+      query: `
+      {
+        viewer {
+          name
+        }
+      }
+    `,
+    };
+
+    fetch(github.baseURL, {
+      method: "POST",
+      headers: github.headers,
+      body: JSON.stringify(githubQuery),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
     <div className="App container mt-5">
       <h1 className="text-primary">
