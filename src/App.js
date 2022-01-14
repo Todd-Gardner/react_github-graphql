e15253e9c672f935e89a7ac34795c8e79c57929a
Todd-Gardner <Todd-Gardner@users.noreply.github.com>
2022-01-14 14:59:48 -0500

    Seperated graphQL query(s) to own module. Display list of repos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import github from "./db";
+import query from "./Query";
 
 function App() {
-  useEffect(() => {
-    const githubQuery = {
-      query: `
-      {
-        viewer {
-          name
-        }
-      }
-    `,
-    };
+  // better to just save as user {} in one state ?
+  const [userName, setUserName] = useState("");
+  const [repoList, setRepoList] = useState([]); //null
 
+  useEffect(() => {
     fetch(github.baseURL, {
       method: "POST",
       headers: github.headers,
-      body: JSON.stringify(githubQuery),
+      body: JSON.stringify(query),
     })
       .then((response) => response.json())
-      .then((data) => console.log(data))
+      .then((data) => {
+        //console.log(data);
+        const viewer = data.data.viewer;
+        setUserName(viewer.name);
+        setRepoList(viewer.repositories.nodes);
+      })
       .catch((err) => console.error(err));
   }, []);
 
@@ -29,6 +29,20 @@ function App() {
         <i className="bi bi-diagram-2-fill" />
         Repos
       </h1>
+      <p>Welcome {userName}</p>
+
+      {repoList && (
+        <ul className="list-group list-group-flush">
+          {repoList.map((repo) => (
+            <li className="list-group-item" key={repo.id.toString()}>
+              <a className="h5 mb-0 text-decoration-none" href={repo.url}>
+                {repo.name}
+              </a>
+              <p className="small">Description: {repo.description}</p>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
